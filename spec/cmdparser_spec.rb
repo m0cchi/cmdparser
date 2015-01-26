eda28a23eaa875603c93b0b7f49ad2ebd28783a6
mocchi <boom.boom.planet@gmail.com>
2015-01-26 13:38:00 +0900

    FIX: spec file

--- spec/cmdparser_spec.rb
+++ spec/cmdparser_spec.rb
@@ -5,7 +5,21 @@ describe Cmdparser do
     expect(Cmdparser::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'on' do
+    it 'usage' do
+      CmdParser.new{
+        on ['help','-h'] do
+          print "help command\n"
+        end
+        on 'server' do
+        end
+        on 'server',['port','-p'] do |port|
+          print "port #{port}\n"
+        end
+        on 'server',['type','-t'] do |type|
+          print "type #{type}\n"
+        end
+      }.invoke
+    end
   end
 end
